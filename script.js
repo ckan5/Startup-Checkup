d6f334c94424118f5ac0df2917dcdf5cab3e28a1
ckan5 <kittykancarina@gmail.com>
2017-01-04 00:14:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -27,13 +27,7 @@ function validateForm() {
     alert("Number of shares owned must be at least 1");
     return false;
   }
-  
-  if (letterform.attorneyYN.checked == true) {
-    alert("This site is not meant for those with attorneys. Please verify that you do not have an attorney.");
-    letterform.attorneyYN.focus();
-    return false;
-  }
-  
+    
   if (letterform.p1.checked == false && letterform.p2.checked == false && letterform.p3.checked == false && letterform.p4.checked == false && letterform.p5.checked == false) {
     alert("At least one purpose must be checked.");
     letterform.p5.focus();
